a9ff27dc68c273e7d1fb89562f8fb7dda26f49e0
abdiysf <mustafeabdisalam@gmail.com>
2024-10-30 21:17:55 +0300

    Initial Commit

---
+++ jokesApp/jokesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  jokesAppApp.swift
+//  jokesApp
+//
+//  Created by Abdirahman Standard on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct jokesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
